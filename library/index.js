bebba512cc6352f53dc32e6cc3f2112ab86f77b3
Oksana-bondareva <oksanchik252@gmail.com>
2023-08-08 01:13:50 +0300

    feat: correct adaptive featuresfor finish task

--- library/index.js
+++ library/index.js
@@ -1,5 +1,11 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menuNav = document.querySelector('.nav-menu');
 menuBtn.addEventListener('click', function(){
-	menuNav.classList.toggle('active');
+	menuNav.classList.toggle('open');
 })
+
+function burgerMenu(icon) {
+    icon.classList.toggle('open');
+}
+
+
