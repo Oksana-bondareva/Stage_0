0615e7a9ebb2e0ad77b137740778d2057aa38666
Oksana-bondareva <oksanchik252@gmail.com>
2023-08-25 01:37:45 +0300

    feat: add slaider for scrin > 1024px

--- library/index.js
+++ library/index.js
@@ -8,34 +8,90 @@ function burgerMenu(icon) {
     icon.classList.toggle('open');
 }
 
-if (document.body.clientWidth <= 1024) {
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+if (document.body.clientWidth > 1024) {
+  let position = 0;
+  let slider = document.querySelector(".img-slaider-conteiner");
+  let dots1 = document.querySelector(".pagination1");
+  let dots2 = document.querySelector(".pagination2");
+  let dots3 = document.querySelector(".pagination3");
   
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("img-slaider");
-    let dots = document.getElementsByClassName("pagination");
-    if (n > slides.length) {slideIndex = slides.length}    
-    if (n < 1) {slideIndex = 1}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" activ-pagination", "");
+  dots1.addEventListener('click', () => {
+    position = 480;
+    position -= 480;
+    slider.style.left = -position + 'px';
+    if(position = 1920) {
+      position = 0;
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " activ-pagination";
-  }
+  });
+
+  dots2.addEventListener('click', () => {
+    position = 480;
+    slider.style.left = -position + 'px';
+    if(position  = 0) {
+      position += 960;
+    } 
+    if(position  = 960) {
+      position -= 480;
+    }
+  });
+  
+  dots3.addEventListener('click', () => {
+    position = 480;
+    position += 480;
+    slider.style.left = -position + 'px';
+    if(position = 0) {
+      position = 1920;}
+      if(position  = 480) {
+        position = 0;
+      }
+      if(position  <= 480) {
+        position = 960;
+      }
+  });
+
+    dots1.onclick = function(){
+    this.className ="activ-pagination";
+    dots2.className ="pagination2";
+    dots3.className ="pagination3";
+};
+
+  dots2.onclick = function(){
+  this.className ="activ-pagination";
+  dots1.className ="pagination1";
+  dots3.className ="pagination3";
+};
+
+  dots3.onclick = function(){
+  this.className ="activ-pagination";
+  dots1.className ="pagination1";
+  dots2.className ="pagination2";
+};
 }
   else {
+    let slideIndex = 1;
+    showSlides(slideIndex);
     
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
+      
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+      
+      function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("img-slaider");
+        let dots = document.getElementsByClassName("pagination");
+        if (n > slides.length) {slideIndex = slides.length}    
+        if (n < 1) {slideIndex = 1}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+          }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" activ-pagination", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " activ-pagination";
+      } 
 };
