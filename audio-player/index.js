054108b81fb353d908ab25ee7392a1fa00ad0244
Oksana-bondareva <oksanchik252@gmail.com>
2023-09-20 20:57:06 +0300

    feat: add funcion for next song

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,8 +1,8 @@
-const song = document.getElementById('song');
+const song = document.getElementById("song");
 const background = document.getElementById("background");
 const playerFon = document.getElementById("player-fon");
-const singer = document.getElementById("singer");
-const songName = document.getElementById("song-name");
+const singer = document.querySelector(".singer");
+const songName = document.querySelector(".song-name");
 let progress = document.getElementById("progress");
 let durationTime = document.getElementById("duration-time");
 let currentTime = document.getElementById("current-time");
@@ -27,6 +27,54 @@ function playAudio() {
       playerFon.style.transform = "scale(1)";
     }
   };
+
+  songIndex = 0;
+
+  let songs = [
+    "./assets/audio/J_Balvin_and_Willy_William_-_Mi_Gente_50236773.mp3",
+    "./assets/audio/Arilena_Ara_-_Nentori_Beverly_Pills_Remix_46331815.mp3",
+    "./assets/audio/Florida_-_Gfdr_OST_forsazh_7_45766929.mp3"
+  ];
+
+  let playerFones = [
+    "./assets/img/maxresdefault1.jpg",
+    "./assets/img/maxresdefault.jpg",
+    "./assets/img/img3.png"
+  ];
+
+  let singers = [
+    "J Balvin, Willy William",
+    "Arilena Ara",
+    "Florida"
+  ];
+
+  let songNames = [
+    "Me Gente",
+    "Nentori",
+    "Gfdr"
+  ];
+
+  function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+      songIndex = 0;
+    }
+    song.src = songs[songIndex];
+    playerFon.src = playerFones[songIndex];
+    background.src = playerFones[songIndex];
+    singer.textContent = singers[songIndex];
+    songName.textContent = songNames[songIndex];
+    if (isPlay = true) {
+      playButton.style.display = "block";
+      pauseButton.style.display = "none";
+      isPlay = false;
+      song.pause();
+      playerFon.style.transform = "scale(1)";
+    }
+  }
+
+  
+
 
 
 
